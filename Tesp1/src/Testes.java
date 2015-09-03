bff08b7aef13b131b8645358a9058fc1fd69852b
11320839 <11320839@ESTB80704.unibh.edu>
2015-09-02 21:53:10 -0300

    teste

--- Tesp1/src/Testes.java
+++ Tesp1/src/Testes.java
@@ -11,108 +11,94 @@ import br.unibh.persistencia.AlunoDAO;
 import br.unibh.persistencia.ProfessorDAO;
 
 public class Testes {
-	
-	
+
 	@Test
-	public void findAluno(){
+	public void findAluno() {
 		AlunoDAO dao = new AlunoDAO();
 		Aluno a = dao.find(2L);
-		Assert.assertEquals(a.getNome(), "Mauro Jose");
+		Assert.assertEquals(a.getNome(), "José Pereira");
 	}
-	
-	
+	/*
 	@Test
-	public void insertAluno(){
+	public void insertAluno() {
 		AlunoDAO dao = new AlunoDAO();
 		Aluno a = new Aluno(null, "Wilson Freitas", "35345678951", "12311115", new Date());
 		dao.insert(a);
-		Aluno a2 = dao.find(9L);
+		Aluno a2 = dao.find(104L);
 		Assert.assertEquals(a2.getNome(), "Wilson Freitas");
 	}
-	
 
-	
 	@Test
-	public void excluirAluno(){
+	public void excluirAluno() {
 		AlunoDAO dao = new AlunoDAO();
 		Aluno a = dao.find(8L);
 		dao.delete(a);
-		List <Aluno> lista = dao.findAll();
-		Assert.assertEquals(lista.size(), 5);
+		List<Aluno> lista = dao.findAll();
+		Assert.assertEquals(lista.size(), 102);
 	}
-	
-	
+
 	@Test
-	public void listarAlunos(){
+	public void listarAlunos() {
 		AlunoDAO dao = new AlunoDAO();
 		List<Aluno> lista = dao.findAll();
-		Assert.assertEquals(lista.size(), 6);
+		Assert.assertEquals(lista.size(), 103);
 	}
-	
+
 	@Test
-	public void alterarAluno(){
+	public void alterarAluno() {
 		AlunoDAO dao = new AlunoDAO();
 		Aluno a = dao.find(8L);
 		a.setNome("Evaristo Costa");
 		dao.update(a);
 		Aluno a2 = dao.find(8L);
 		Assert.assertEquals(a2.getNome(), "Evaristo Costa");
-	
+
 	}
-	
-	
+
 	// TESTES DA CLASSE PROFESSOR
-	
+
 	@Test
-	public void findProfessor(){
+	public void findProfessor() {
 		ProfessorDAO daoProf = new ProfessorDAO();
 		Professor p = daoProf.find(3L);
-		Assert.assertEquals(p.getNome(), "Felipe Antonio");
+		Assert.assertEquals(p.getNome(), "Carlos Tavares");
 	}
-	
-	
-	
+
 	@Test
-	public void alteraProfessor(){
+	public void alteraProfessor() {
 		ProfessorDAO daoProf = new ProfessorDAO();
 		Professor p = daoProf.find(2L);
 		p.setNome("Silvio Santos");
 		daoProf.update(p);
 		Professor p1 = daoProf.find(2L);
 		Assert.assertEquals(p1.getNome(), "Silvio Santos");
-		
+
 	}
-	
-	
-	
-	
+
 	@Test
-	public void excluirProfessor(){
+	public void excluirProfessor() {
 		ProfessorDAO daoProf = new ProfessorDAO();
 		Professor p = daoProf.find(4L);
 		daoProf.delete(p);
-		List <Professor> listaprof = daoProf.findAll();
-		Assert.assertEquals(listaprof.size(), 2);
+		List<Professor> listaprof = daoProf.findAll();
+		Assert.assertEquals(listaprof.size(), 103);
 	}
-	
-	
-	
-	
+
 	@Test
-	public void findAllProfessor(){
+	public void findAllProfessor() {
 		ProfessorDAO daoProf = new ProfessorDAO();
-		List <Professor> listaprof = daoProf.findAll();
-		Assert.assertEquals(listaprof.size(), 3);
+		List<Professor> listaprof = daoProf.findAll();
+		Assert.assertEquals(listaprof.size(), 104);
 	}
-	
+
 	@Test
-	public void insereProfessor(){
+	public void insereProfessor() {
 		ProfessorDAO daoProf = new ProfessorDAO();
 		Professor p = new Professor(null, "Alberto D", "33389878787", new BigDecimal("30000"));
 		daoProf.insert(p);
-		Professor p1 = daoProf.find(9L);
+		Professor p1 = daoProf.find(105L);
 		Assert.assertEquals(p1.getNome(), "Alberto D");
 	}
-	
+	*/
 
 }
--- Tesp1/src/br/unibh/persistencia/AlunoDAO.java
+++ Tesp1/src/br/unibh/persistencia/AlunoDAO.java
@@ -2,20 +2,18 @@ package br.unibh.persistencia;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Types;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
-
-
 
 import br.unibh.persistencia.JDBCUtil;
 import br.unibh.entidades.Aluno;
 
 public class AlunoDAO implements DAO<Aluno, Long> {
 	public static SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-	
+
+	@Override
 	public Aluno find(Long id) {
 		try {
 			PreparedStatement p = JDBCUtil.getConnection().prepareStatement("select * from tb_aluno where id = ?");
@@ -24,8 +22,8 @@ public class AlunoDAO implements DAO<Aluno, Long> {
 
 			if (res.next()) {
 				return new Aluno(res.getLong("id"), res.getString("nome"), res.getString("cpf"),
-						res.getString("matricula"), res.getString("data_aniversario") == null ? null : df
-								.parse(res.getString("data_aniversario")));
+						res.getString("matricula"),
+						res.getString("data_aniversario") == null ? null : df.parse(res.getString("data_aniversario")));
 			}
 
 		} catch (Exception e) {
@@ -36,51 +34,76 @@ public class AlunoDAO implements DAO<Aluno, Long> {
 		return null;
 	}
 
-	public void insert(Aluno t) {
-		// TODO Auto-generated method stub
-				try {
-					PreparedStatement p = JDBCUtil.getConnection().prepareStatement(
-							"insert into tb_aluno (nome, cpf, matricula, data_aniversario) values (?,?,?,?)");
-					p.setString(1, t.getNome());
-					p.setString(2, t.getCpf());
-					p.setString(3, t.getMatricula());
-					p.setString(4, df.format(t.getData_aniversario()));
-					p.executeUpdate();	
+	public Aluno find(String nome) {
+		try {
+			PreparedStatement p = JDBCUtil.getConnection()
+					.prepareStatement("select * from tb_aluno where nome like ? + '%'");
+			p.setString(1, nome);
+			ResultSet res = p.executeQuery();
 
-				} catch (Exception e) {
-					e.printStackTrace();
-				} finally {
-					JDBCUtil.closeConnection();
-				}
-		
+			if (res.next()) {
+				return new Aluno(res.getLong("id"), res.getString("nome"), res.getString("cpf"),
+						res.getString("matricula"),
+						res.getString("data_aniversario") == null ? null : df.parse(res.getString("data_aniversario")));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.closeConnection();
+		}
+		return null;
 	}
 
+	@Override
+	public void insert(Aluno t) {
+		// TODO Auto-generated method stub
+		try {
+			PreparedStatement p = JDBCUtil.getConnection()
+					.prepareStatement("insert into tb_aluno (nome, cpf, matricula, data_aniversario) values (?,?,?,?)");
+			p.setString(1, t.getNome());
+			p.setString(2, t.getCpf());
+			p.setString(3, t.getMatricula());
+			p.setString(4, df.format(t.getData_aniversario()));
+			p.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCUtil.closeConnection();
+		}
+
+	}
+
+	@Override
 	public void update(Aluno t) {
-try {
-			
-			String sql = ("update tb_aluno set nome = ?, "	+ "cpf = ?, matricula = ?, data_aniversario = ? " + "where id = ?");
+		try {
+
+			String sql = ("update tb_aluno set nome = ?, " + "cpf = ?, matricula = ?, data_aniversario = ? "
+					+ "where id = ?");
 			PreparedStatement comando = JDBCUtil.getConnection().prepareStatement(sql);
 			comando.setString(1, t.getNome());
 			comando.setString(2, t.getCpf());
 			comando.setString(3, t.getMatricula());
-			if(t.getData_aniversario() == null){
+			if (t.getData_aniversario() == null) {
 				comando.setNull(4, Types.NULL);
-			}else{
+			} else {
 				comando.setString(4, df.format(t.getData_aniversario()));
-				
+
 			}
 			comando.setLong(5, t.getId());
 			comando.executeUpdate();
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally{
+		} finally {
 			JDBCUtil.closeConnection();
 		}
-		
+
 	}
 
+	@Override
 	public void delete(Aluno t) {
 		try {
 			String sql = ("delete from tb_aluno where id =?");
@@ -89,20 +112,21 @@ try {
 			comando.executeUpdate();
 		} catch (Exception e) {
 			// TODO: handle exception
-		}finally{
+		} finally {
 			JDBCUtil.closeConnection();
 		}
-		
+
 	}
 
+	@Override
 	public List<Aluno> findAll() {
 		ArrayList<Aluno> lista = new ArrayList<Aluno>();
 		try {
 			ResultSet res = JDBCUtil.getConnection().prepareStatement("select * from tb_aluno").executeQuery();
 			while (res.next()) {
 				lista.add(new Aluno(res.getLong("id"), res.getString("nome"), res.getString("cpf"),
-						res.getString("matricula"), res.getString("data_aniversario") == null ? 
-								null : df.parse(res.getString("data_aniversario"))));
+						res.getString("matricula"), res.getString("data_aniversario") == null ? null
+								: df.parse(res.getString("data_aniversario"))));
 			}
 
 		} catch (Exception e) {
@@ -113,5 +137,4 @@ try {
 		return lista;
 	}
 
-	
 }
